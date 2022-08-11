9bf95e68834b702e1114aa26f0ba6f8a6e92362e
dplamenov <dimitar.plamenov@gmail.com>
2022-08-11 21:12:30 +0300

    Update lottery.js

--- scripts/lottery.js
+++ scripts/lottery.js
@@ -1,8 +1,14 @@
 const hre = require("hardhat");
 
+const VRFConsumerBaseData = {
+  vrfCoordinator: '0x271682DEB8C4E0901D1a1550aD2e64D568E69909',
+  link: '0x514910771af9ca656af840dff83e8264ecf986ca',
+  keyHash: '0x9fe0eebf5e446e3c998ec9bb19951541aee00bb90ea201ae456421a2ded86805'
+};
+
 async function main() {
   const RandomWinner = await hre.ethers.getContractFactory("RandomWinner");
-  const randomWinner = await RandomWinner.deploy("0x271682DEB8C4E0901D1a1550aD2e64D568E69909", "0x514910771af9ca656af840dff83e8264ecf986ca", "0x9fe0eebf5e446e3c998ec9bb19951541aee00bb90ea201ae456421a2ded86805");
+  const randomWinner = await RandomWinner.deploy(VRFConsumerBaseData.vrfCoordinator, VRFConsumerBaseData.link, VRFConsumerBaseData.keyHash);
 
   await randomWinner.deployed();
 
