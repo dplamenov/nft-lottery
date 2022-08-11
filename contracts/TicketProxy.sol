caa63aee9fd3fa1e007acbfd4cbbefb00f46afc1
dplamenov <dimitar.plamenov@gmail.com>
2022-08-11 16:05:41 +0300

    use Beacon Proxy pattern

---
+++ contracts/TicketProxy.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
+
+contract TicketProxy is BeaconProxy {
+    constructor(address _beacon) payable BeaconProxy(_beacon, "") {}
+
+    function beacon() public view returns (address) {
+        return _beacon();
+    }
+
+    function implementation() public view returns (address) {
+        return _implementation();
+    }
+}
