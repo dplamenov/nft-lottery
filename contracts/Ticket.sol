2fc50cd2f2c3a9705610f10a975992fb1c247f04
dplamenov <dimitar.plamenov@gmail.com>
2022-08-11 14:05:39 +0300

    Update Ticket.sol

--- contracts/Ticket.sol
+++ contracts/Ticket.sol
@@ -1,4 +1,31 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Ticket {}
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+
+contract Ticket is ERC721Upgradeable {
+    uint64 public startBlock;
+    uint64 public endBlock;
+    uint256 public ticketPrice;
+
+    string baseURI;
+
+    function initialize(
+        string memory _name,
+        string memory _symbol,
+        string memory _baseURI,
+        uint64 _start,
+        uint64 _end,
+        uint128 _ticketPrice
+    ) external initializer {
+        __ERC721_init(_name, _symbol);
+        baseURI = _baseURI;
+        startBlock = _start;
+        endBlock = _end;
+        ticketPrice = _ticketPrice;
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseURI;
+    }
+}
