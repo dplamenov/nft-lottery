9ae3e493cbf0efdf307b44f5006559f640a6465e
dplamenov <dimitar.plamenov@gmail.com>
2022-08-11 14:13:01 +0300

    Update Ticket.sol

--- contracts/Ticket.sol
+++ contracts/Ticket.sol
@@ -3,12 +3,21 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 
+error InsufficientAmount();
+
 contract Ticket is ERC721Upgradeable {
     uint64 public startBlock;
     uint64 public endBlock;
     uint256 public ticketPrice;
+    uint256 public tokenCount;
 
     string baseURI;
+
+    modifier active() {
+        if (block.number < startBlock || block.numer > endBlock)
+            revert Unavailable();
+        _;
+    }
 
     function initialize(
         string memory _name,
@@ -23,6 +32,12 @@ contract Ticket is ERC721Upgradeable {
         startBlock = _start;
         endBlock = _end;
         ticketPrice = _ticketPrice;
+    }
+
+    function buyTicket() {
+        if (msg.value < ticketPrice) revert InsufficientAmount();
+        _mint(msg.sender, tokenCount);
+        ++tokenCount;
     }
 
     function _baseURI() internal view virtual override returns (string memory) {
