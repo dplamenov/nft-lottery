5f1ba98741c6b318b041f31616108840768c2e0c
dplamenov <dimitar.plamenov@gmail.com>
2022-08-12 11:01:07 +0300

    create ITicket interface

---
+++ contracts/ITicket.sol
@@ -0,0 +1,20 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface ITicket {
+    function initialize(
+        string memory _name,
+        string memory _symbol,
+        string memory _baseURI,
+        uint64 _startBlock,
+        uint64 _endBlock,
+        uint128 _ticketPrice,
+        address payable _randomWinnerAddress
+    ) external;
+
+    function buyTicket() external payable;
+
+    function pickWinner() external;
+
+    function win(uint256 randomness) external payable;
+}
