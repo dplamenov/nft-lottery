49ab6d8139c5ddd2a102ba6b44f4c534fb0cdb18
dplamenov <dimitar.plamenov@gmail.com>
2022-08-12 10:51:17 +0300

    Create VRFConsumerBaseData.js

---
+++ test/utils/VRFConsumerBaseData.js
@@ -0,0 +1,5 @@
+export default VRFConsumerBaseData = {
+  vrfCoordinator: '0x271682DEB8C4E0901D1a1550aD2e64D568E69909',
+  link: '0x514910771af9ca656af840dff83e8264ecf986ca',
+  keyHash: '0x9fe0eebf5e446e3c998ec9bb19951541aee00bb90ea201ae456421a2ded86805'
+};
