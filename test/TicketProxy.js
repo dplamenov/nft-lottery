29f8d08065ede7d81004ac631af869a524ea9902
dplamenov <dimitar.plamenov@gmail.com>
2022-08-13 09:12:51 +0300

    Create TicketProxy.js

---
+++ test/TicketProxy.js
@@ -0,0 +1,45 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const { deployMockedRandomWinner } = require("./utils/mock");
+
+describe('TicketProxy', async function () {
+  let TicketBeacon;
+  let TicketProxy;
+  let Ticket;
+
+  beforeEach(async function () {
+    [deployer] = await ethers.getSigners();
+
+    RandomWinner = await deployMockedRandomWinner();
+    ticketData = ['Ticket', 'T1', 'test', 0, 150, 1000000000, RandomWinner.address];
+
+    Ticket = await (await ethers.getContractFactory("Ticket")).deploy();
+    await Ticket.initialize(...ticketData);
+
+    TicketBeacon = await (await ethers.getContractFactory("TicketBeacon")).deploy(Ticket.address);
+    TicketProxy = await (await ethers.getContractFactory("TicketProxy")).deploy(TicketBeacon.address);
+  });
+
+  describe("Test beacon", async function () {
+    it('Should return correct beacon', async () => {
+      expect(await TicketProxy.beacon()).to.be.eq(TicketBeacon.address);
+    });
+  });
+
+  describe("Test implementation", async function () {
+    it('Should return correct implementation', async () => {
+      expect(await TicketProxy.implementation()).to.be.eq(Ticket.address);
+    });
+  });
+
+  describe("New implementation", async function () {
+    it('Should return new implementation', async () => {
+      const Ticket = await (await ethers.getContractFactory("Ticket")).deploy();
+      await Ticket.initialize(...ticketData);
+
+      await TicketBeacon.upgradeTo(Ticket.address);
+
+      expect(await TicketProxy.implementation()).to.be.eq(Ticket.address);
+    });
+  });
+});
